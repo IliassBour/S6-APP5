be5f67418e9caf7c7bd9f93a0c41b5349b7cc4cd
Iliass b <iliass.bourabaa@usherbrooke.ca>
2022-07-10 16:15:31 -0400

    Poll archive every 5 sec

--- interface/src/BeaconList.js
+++ interface/src/BeaconList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './BeaconList.css';
 
 
@@ -14,6 +14,11 @@ function Beacon(props) {
 function BeaconList(props) {
     const [beacons, setBeacons] = React.useState(props.beacons);
 
+    useEffect(() => {
+        const timer = setInterval(refreshList, 5000);
+        return () => clearInterval(timer);
+    }, []);
+
     function refreshList() {
         const xhttp = new XMLHttpRequest();
         xhttp.open("GET", "http://localhost:3006/deviceUuid", false);
@@ -27,7 +32,6 @@ function BeaconList(props) {
 
     return(
         <div className="beaconList">
-            <button onClick={refreshList}>Refresh</button>
             {beacons.map(b => <Beacon key={b.id} deviceUuid={b.deviceUuid}/>)}
         </div>
     )
